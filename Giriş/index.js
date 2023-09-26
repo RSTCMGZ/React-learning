154b6a3f0386b2bc9c04d86256c0c005e406abf6
Reşat Çamgöz <114862309+RSTCMGZ@users.noreply.github.com>
2023-09-26 22:00:46 +0300

    learn-react
    learn-react

---
+++ Giriş/index.js
@@ -0,0 +1,6 @@
+console.log("Hello World");
+
+var slugify = require('slugify')
+
+let merhaba = slugify('Benim Adım Reso', "/")
+console.log(merhaba);
---
+++ Giriş/topla.js
@@ -0,0 +1,3 @@
+let x = 5;
+let y = 12;
+console.log(x + y);
